1de6045d4222bba89029df877d2920bf4fd10f1e
neeraj kumar <88071981+Nyrexcoder@users.noreply.github.com>
2022-07-17 10:11:46 +0530

    delete

--- routes/web.js
+++
@@ -1,8 +0,0 @@
-const express =  require('express')
-const blogsController = require('../controllers/blogs.js');
-const router = express.Router();
-
-router.get('/blogs',blogsController.getAllDoc)
-
-
-module.exports = router
